88e84f0efb17c2c7d342f95ae5a94511b326a0fc
otisdev95 <olaliotis95@gmail.com>
2023-06-21 00:17:42 +0100

    AirBnB clone - MySQL

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Do_pack defination"""
+
+from fabric.api import local
+from time import strftime
+from datetime import date
+
+
+def do_pack():
+    """ Script that generates an archive for the contents of web_static folder"""
+
+    filename = strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(filename))
+
+        return "versions/web_static_{}.tgz".format(filename)
+
+    except Exception as e:
+        return None
--- README.md
+++ README.md
@@ -5,6 +5,6 @@
 <p>The project currently only implements the back-end console.</p>
 <h3>Authors:</h3>
 <ul>
-	<li>Emmauel Daniel https://github.com/ogbuman</li>
+	<li>Emmanuel Daniel https://github.com/ogbuman</li>
 	<li>Otis Olali https://github.com/otisdev95</li>
 </ul>
